cb6b7c7dcc5cdcc63a4698da8d06f6d93f30ce6f
williamorim <thewilliam89@gmail.com>
2023-04-05 20:34:51 -0300

    fazendo logica do server funcionar

--- R/shiny.R
+++ R/shiny.R
@@ -10,10 +10,15 @@ uiFirebase <- function(ui) {
       firebaseAdmin <- init_firebase_admin()
       credentials <- firebaseAdmin$credentials$Certificate("serviceAccountKey.json")
       firebaseAdmin$initialize_app(
-        credentials,
-        list('databaseURL'= NULL)
+        credential = credentials,
+        options = list('databaseURL'= NULL),
+        name = "ui"
       )
-      res <- try(firebaseAdmin$auth$verify_id_token(token), silent = TRUE)
+      res <- try(
+        firebaseAdmin$auth$verify_id_token(token, firebaseAdmin$get_app("ui")),
+        silent = TRUE
+      )
+      firebaseAdmin$delete_app(firebaseAdmin$get_app("ui"))
       if (class(res) == "try-error") {
         shiny::tags$script(shiny::HTML('location.replace("login/index.html");'))
       } else {
@@ -28,9 +33,62 @@ uiFirebase <- function(ui) {
 }
 
 serverFirebase <- function(server) {
-  server
+  function(input, output, session) {
+
+    appname <- paste0("server", runif(1))
+    firebaseAdmin <- init_firebase_admin()
+    firebaseAdmin$initialize_app(
+      credential = firebaseAdmin$credentials$Certificate("serviceAccountKey.json"),
+      options = list('databaseURL'= NULL),
+      name = appname
+    )
+
+    observe({
+      token <- cookies::get_cookie("firebase_token")
+      session$userData$firebase <- firebaseAdmin$auth$verify_id_token(
+        token,
+        app = firebaseAdmin$get_app(appname)
+      )
+    })
+
+    observeEvent(input$._firebaselogout_, {
+      firebaseAdmin$auth$revoke_refresh_tokens(
+        session$userData$firebase$uid,
+        app = firebaseAdmin$get_app(appname)
+      )
+      firebaseLogout()
+    })
+
+    server(input, output, session)
+
+    onStop(
+      function() {
+        firebaseAdmin$delete_app(firebaseAdmin$get_app(appname))
+      }
+    )
+
+  }
 }
 
 shinyAppFirebase <- function(ui, server, ...) {
-  shiny::shinyApp(uiFirebase(ui), serverFirebase(server), ...)
+  shiny::shinyApp(
+    ui = uiFirebase(ui),
+    server = serverFirebase(server),
+    ...
+  )
+}
+
+firebaseLogoutButton <- function(label = "Log out", ..., id = "._firebaselogout_") {
+  shiny::actionButton(id, label, ...)
+}
+
+firebaseLogout <- function() {
+  shiny::insertUI(
+    selector = "head",
+    where = "beforeEnd",
+    immediate = TRUE,
+    ui = shinyjs::useShinyjs()
+  )
+  js <- 'location.replace("login/index.html");'
+  shinyjs::runjs(js)
 }
--- inst/app.R
+++ inst/app.R
@@ -2,11 +2,26 @@ library(shiny)
 devtools::load_all()
 
 ui <- fluidPage(
-  titlePanel("It works!")
+  titlePanel("It works!"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        "xvar",
+        "X axis",
+        choices = names(mtcars)
+      ),
+      firebaseLogoutButton()
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
 )
 
 server <- function(input, output, session) {
-
+  output$plot <- renderPlot({
+    plot(x = mtcars[[input$xvar]], y = mtcars$mpg)
+  })
 }
 
 shinyAppFirebase(ui, server)
