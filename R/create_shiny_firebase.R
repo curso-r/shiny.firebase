96c5f79c92940398e8db01730e5a89b53a64c949
williamorim <thewilliam89@gmail.com>
2023-04-18 22:01:48 -0300

    function to create firebase app files

--- R/create_shiny_firebase.R
+++ R/create_shiny_firebase.R
@@ -1,7 +1,7 @@
 #' Create shiny firebase app
 #'
-#' @param path
-#' @param overwrite
+#' @param path Path where the app file will be created.
+#' @param overwrite Logical. Overwrite files if they exist. Defaults to FALSE.
 #'
 #' @export
 #'
@@ -10,4 +10,5 @@ create_shiny_firebase <- function(path, overwrite = FALSE) {
   from <- system.file("app_template/", package = "shiny.firebase")
   files_to_copy <- list.files(from, full.names = TRUE)
   file.copy(files_to_copy, to = path, overwrite = overwrite, recursive = TRUE)
+  invisible(TRUE)
 }
