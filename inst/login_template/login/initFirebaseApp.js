9a4fa64aa350242d2849c8541fe6341be13c5622
williamorim <thewilliam89@gmail.com>
2023-04-18 22:02:37 -0300

    login page template

---
+++ inst/login_template/login/initFirebaseApp.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+      apiKey: "{{apiKey}}",
+      authDomain: "{{authDomain}}",
+      databaseURL: "{{databaseURL}}",
+      projectId: "{{projectId}}",
+      storageBucket: "{{storageBucket}}",
+      messagingSenderId: "{{messagingSenderId}}",
+      appId: "{{appId}}",
+      measurementId: "{{measurementId}}"
+    };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
