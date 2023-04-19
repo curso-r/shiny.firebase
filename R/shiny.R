cc69ee2ed7ad4eed8b1548bee2dd3b9838a487ca
williamorim <thewilliam89@gmail.com>
2023-04-18 22:02:10 -0300

    documentation

--- R/shiny.R
+++ R/shiny.R
@@ -1,10 +1,28 @@
+#' Create a Firebase Shiny app object
+#'
+#' @param ui The ui object of your app.
+#' @param server Ther server function of your app.
+#' @param ... Additional parameters passed to shiny::shinyApp function.
+#'
+#' @return An object that represents the app. Printing the object or passing
+#' it to runApp() will run the app.
+#' @export
+#'
+shinyAppFirebase <- function(ui, server, ...) {
+  shiny::shinyApp(
+    ui = uiFirebase(ui),
+    server = serverFirebase(server),
+    ...
+  )
+}
+
 uiFirebase <- function(ui) {
   function(req) {
     token <- try(
       parseCookies(req$HEADERS['cookie'])$firebase_token,
       silent = TRUE
     )
-    if (class(a) == "try-error" | is.null(token)) {
+    if (methods::is(token, "try-error") | is.null(token)) {
       shiny::tags$script(shiny::HTML('location.replace("login/index.html");'))
     } else {
       firebaseAdmin <- init_firebase_admin()
@@ -23,7 +41,7 @@ uiFirebase <- function(ui) {
         silent = TRUE
       )
       firebaseAdmin$delete_app(firebaseAdmin$get_app("ui"))
-      if (class(res) == "try-error") {
+      if (methods::is(res, "try-error")) {
         shiny::tags$script(shiny::HTML('location.replace("login/index.html");'))
       } else {
         if (is.function(ui)) {
@@ -39,7 +57,7 @@ uiFirebase <- function(ui) {
 serverFirebase <- function(server) {
   function(input, output, session) {
 
-    appname <- paste0("server", runif(1))
+    appname <- paste0("server", stats::runif(1))
     firebaseAdmin <- init_firebase_admin()
     firebaseAdmin$initialize_app(
       credential = firebaseAdmin$credentials$Certificate("serviceAccountKey.json"),
@@ -47,7 +65,7 @@ serverFirebase <- function(server) {
       name = appname
     )
 
-    observe({
+    shiny::observe({
       token <- cookies::get_cookie("firebase_token")
       session$userData$firebase <- firebaseAdmin$auth$verify_id_token(
         token,
@@ -55,7 +73,7 @@ serverFirebase <- function(server) {
       )
     })
 
-    observeEvent(input$._firebaselogout_, {
+    shiny::observeEvent(input$._firebaselogout_, {
       firebaseAdmin$auth$revoke_refresh_tokens(
         session$userData$firebase$uid,
         app = firebaseAdmin$get_app(appname)
@@ -65,7 +83,7 @@ serverFirebase <- function(server) {
 
     server(input, output, session)
 
-    onStop(
+    shiny::onStop(
       function() {
         firebaseAdmin$delete_app(firebaseAdmin$get_app(appname))
       }
@@ -74,18 +92,28 @@ serverFirebase <- function(server) {
   }
 }
 
-shinyAppFirebase <- function(ui, server, ...) {
-  shiny::shinyApp(
-    ui = uiFirebase(ui),
-    server = serverFirebase(server),
-    ...
-  )
-}
-
+#' Firebase logout button
+#'
+#' @param label The contents of the button or link–usually a text label,
+#' but you could also use any other HTML, like an image.
+#' @param ... Named attributes to be applied to the button or link.
+#' @param id An ID for the button. If you only have one logout button in your
+#' app, you do not need to explicitly provide an ID. If you have more than one
+#' logout button, you need to provide a unique ID to each button. When you
+#' create a button with a non-default ID, you must create an observer that
+#' listens to a click on this button and logs out of the app with a call to
+#' logout.
+#'
+#' @export
+#'
 firebaseLogoutButton <- function(label = "Log out", ..., id = "._firebaselogout_") {
   shiny::actionButton(id, label, ...)
 }
 
+#' Logout from Firebase
+#'
+#' @export
+#'
 firebaseLogout <- function() {
   shiny::insertUI(
     selector = "head",
