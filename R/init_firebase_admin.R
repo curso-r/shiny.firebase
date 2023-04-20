7f4ba530521f0e181bdcb7badad6907a7bbee3b1
williamorim <thewilliam89@gmail.com>
2023-04-20 12:12:47 -0300

    error message if firebase_admin is not installed

--- R/init_firebase_admin.R
+++ R/init_firebase_admin.R
@@ -1,4 +1,10 @@
 init_firebase_admin <- function() {
-  reticulate::use_virtualenv("firebase")
-  reticulate::import("firebase_admin")
+  if (!reticulate::virtualenv_exists("firebase") |
+      !"firebase-admin" %in% reticulate::py_list_packages("firebase")$package) {
+    stop("The Python package 'firebase_admin' is not installed.\n
+         Please run 'install_firebase_admin()' before running the application.")
+  } else {
+    reticulate::use_virtualenv("firebase")
+    reticulate::import("firebase_admin")
+  }
 }
